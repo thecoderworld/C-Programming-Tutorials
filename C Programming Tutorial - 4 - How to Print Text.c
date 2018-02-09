1a4c7a00c2d79c4ce202e8ab39a886c1b3edf1fe
thecoderworld <coderworldofficial@gmail.com>
2018-02-09 15:14:52 +0530

    Added

---
+++ C Programming Tutorial - 4 - How to Print Text.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("thecoderworld is nice \n"); 
+	
+	// \n and \t are call escapes sequence in C programming.
+	// \n to crate a new line and \t for tab.	
+	
+	printf("thecoderworld is cool");
+	return 0 ;
+}
+
