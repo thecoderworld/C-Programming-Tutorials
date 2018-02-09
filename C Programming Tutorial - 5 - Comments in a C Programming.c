069f38072b22cb253259d82589e4d28322740406
thecoderworld <coderworldofficial@gmail.com>
2018-02-09 14:48:44 +0530

    Create C Programming Tutorial - 5 - Comments in a C Programming.c

---
+++ C Programming Tutorial - 5 - Comments in a C Programming.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("Comments in a C Programming Language. \n");
+	
+  /* Single line comment*/
+   
+   /*
+   This is how you can add
+   multi line comment.
+   */
+   return 0 ;
+}
